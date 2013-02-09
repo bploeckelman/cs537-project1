e8173bbe6ed95694b6d08c6473c2bdf4da5f6c6c
Brian Ploeckelman <ploeckelman@wisc.edu>
2013-02-09 12:22:40 -0600

    Handling no-arg cd and pwd

--- main.c
+++ main.c
@@ -3,9 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 
-const char *PROMPT = "537sh% ";
-const size_t PROMPT_SIZE = 8;
-const int MAX_INPUT = 512; // excluding \n and \0
+const char  *PROMPT     = "537sh% ";
+const size_t PROMPT_LEN = 8;
+const size_t MAX_INPUT  = 512; // excluding \n and \0
+
+const size_t ERROR_LEN = 23;
+const char  *ERROR_MESSAGE = "An error has occurred\n";
 
 const int INCOMPLETE = 0;
 const int COMPLETE = 1;
@@ -30,6 +33,34 @@ void get_input(char *input)
     input[i] = '\0';
 }
 
+// ---- Handle Built-in Commands ----------------------------------------------
+int handle_builtin(const char *input)
+{
+    if (strcmp(input, "exit") == 0) {
+        return COMPLETE;
+    } else if (strcmp(input, "pwd") == 0) {
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) == NULL) {
+            write(STDERR_FILENO, ERROR_MESSAGE, ERROR_LEN);
+        } else {
+            write(STDOUT_FILENO, cwd, strlen(cwd));
+            write(STDOUT_FILENO, "\n", 1); 
+        }
+    } else if (strcmp(input, "cd") == 0) {
+        // Change to home directory
+        const char *home = getenv("HOME");
+        if (home == NULL)
+            write(STDERR_FILENO, ERROR_MESSAGE, ERROR_LEN);
+        else
+            chdir(home);
+    } else if (strcmp(input, "cd[spaces+][dir]") == 0) {
+        // TODO: this won't actually work... needs moar regex
+        write(STDERR_FILENO, ERROR_MESSAGE, ERROR_LEN);
+    }
+
+    return INCOMPLETE;
+}
+
 
 // ---- Process Input ---------------------------------------------------------
 int process_input(const char *input)
@@ -40,7 +71,8 @@ int process_input(const char *input)
     write(STDOUT_FILENO, input, strlen(input));
     write(STDOUT_FILENO, "\n", 1); 
 
-    // Handle exit command
+    // Handle built-in commands
+    handle_builtin(input);
     if (strcmp(input, "exit") == 0) {
         return COMPLETE;
     }
@@ -52,7 +84,7 @@ int process_input(const char *input)
 // ---- Shell Prompt ----------------------------------------------------------
 void prompt()
 {
-    write(STDOUT_FILENO, PROMPT, PROMPT_SIZE);
+    write(STDOUT_FILENO, PROMPT, PROMPT_LEN);
 }
 
 
